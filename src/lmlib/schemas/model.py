e0d8b0501b0dd6ec1fca705ab8193be88c09e7fe
Aniket Mitra <aniket@unl.global>
2025-06-27 17:40:01 +0200

    Update schema for preallocated vessel config
    Signed-off-by: Aniket Mitra <aniket@unl.global>

--- src/lmlib/schemas/model.py
+++ src/lmlib/schemas/model.py
@@ -1,19 +1,16 @@
-from typing import List, Literal, ClassVar
 from enum import Enum
-from lmlib.schemas.base_model import (
-    EquipmentCapabilityProperty,
-    ParameterSpecification,
-    SpatialDefinition, 
-    WorkflowSpecification, 
-    Area, 
-    Equipment, 
-    PhysicalAsset, 
-    OperationalLocation, 
-    StorageZone, 
-    WorkflowSpecificationNode, 
-    WorkSchedule,
-    EquipmentCapability,
-)
+from typing import Dict, List
+
+from lmlib.schemas.base_model import (Area, Equipment, EquipmentCapability,
+                                      EquipmentCapabilityProperty,
+                                      OperationalLocation,
+                                      ParameterSpecification, PhysicalAsset,
+                                      SpatialDefinition, StorageZone,
+                                      WorkflowSpecification,
+                                      WorkflowSpecificationNode, WorkSchedule)
+from pydantic import Field
+
+
 # Enum for Vessel Type
 class VesselType(Enum):
     WINCHESTER = "winchester"
@@ -24,12 +21,15 @@ class VesselType(Enum):
     SCOUT = "scout"
     T_CLASS = "T-Class"
 
+
 class VesselDeckPositionType(Enum):
     """A position on the deck of a vessel."""
+
     IN_PORT_SIDE = "InPortSide"
     OUTER_PORT_SIDE = "OuterPortSide"
     IN_STARBOARD = "InStarboard"
     OUT_STARBOARD = "OutStarboard"
+
 
 class VesselResponsibility(Enum):
     MONOPILE_TRANSPORT = "MonopileTransport"
@@ -39,13 +39,15 @@ class VesselResponsibility(Enum):
     TENSIONING = "Tensioning"
     MOORING = "Mooring"
 
-class WorkTypeEnum(Enum):    
+
+class WorkTypeEnum(Enum):
     PRODUCTION = "production"
     MAINTENANCE = "maintenance"
     QUALITY = "quality"
     INVENTORY = "inventory"
     MIXED = "mixed"
     TRANSPORT = "transport"
+
 
 class WorkScheduleStateEnum(Enum):
     FORECAST = "forecast"
@@ -73,53 +75,70 @@ class DependencyTypeEnum(Enum):
     NO_LATER_AFTER_END = "noLaterAfterEnd"
     NO_EARLIER_AFTER_END = "noEarlierAfterEnd"
 
+
 class GrillageTypeEnum(Enum):
     REV = "REV"
     SRW = "SRW"
 
+
 class MonopileStateTransition(WorkflowSpecification):
     """A state transition of a monopile transport for offshore wind farm installation."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:MonopileStateTransition;1']] = 'dtmi:digitaltwins:isa95:MonopileStateTransition;1'
-    dependency_type : DependencyTypeEnum
+
+    model_id: str = Field(
+        "dtmi:digitaltwins:isa95:MonopileStateTransition;1", Literal=True
+    )
+    dependency_type: DependencyTypeEnum
+
 
 class FabricationYard(StorageZone):
     """A representation of fabrication yard where the monopiles are stored."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:FabricationYard;1']] = 'dtmi:digitaltwins:isa95:FabricationYard;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:FabricationYard;1", Literal=True)
     spatial_definition: SpatialDefinition
     id: str
-    #monopiles: List["Monopile"]
+    # monopiles: List["Monopile"]
+
 
 class Deck(PhysicalAsset):
     """A representation of deck of a vessel."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:Deck;1']] = 'dtmi:digitaltwins:isa95:Deck;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:Deck;1", Literal=True)
     spatial_definition: SpatialDefinition
 
 
 class MonopileTransport(WorkflowSpecification):
     """A representation of a monopile transport for offshore wind farm installation."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:MonopileTransport;1']] = 'dtmi:digitaltwins:isa95:MonopileTransport;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:MonopileTransport;1", Literal=True)
+
 
 class DeckPosition(OperationalLocation):
     """A position on the deck of a vessel."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:DeckPosition;1']] = 'dtmi:digitaltwins:isa95:DeckPosition;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:DeckPosition;1", Literal=True)
     position_type: VesselDeckPositionType
     spatial_definition: SpatialDefinition
 
 
-class Port(Area):
+class Port(Area, SpatialDefinition):
     """A representation of a port where vessels are docked."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:Port;1']] = 'dtmi:digitaltwins:isa95:Port;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:Port;1", Literal=True)
     spatial_definition: SpatialDefinition
+
 
 class OffshoreWindFarm(Area, SpatialDefinition):
     """A representation for offshore wind farm installation."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:OffshoreWindFarm;1']] = 'dtmi:digitaltwins:isa95:OffshoreWindFarm;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:OffshoreWindFarm;1", Literal=True)
     spatial_definition: SpatialDefinition
     monopiles: List["Monopile"]
 
+
 class Monopile(Equipment):
     """A representation of a monopile for offshore wind farm installation"""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:Monopile;1']] = 'dtmi:digitaltwins:isa95:Monopile;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:Monopile;1", Literal=True)
     name: str
     document_number: str
     revision: int
@@ -140,9 +159,13 @@ class Monopile(Equipment):
     spatial_definition: SpatialDefinition
     equipment_level: str = "unit"
 
+
 class MonopileTransportSchedule(WorkSchedule):
     """A representation of monopile transport schedule for offshore wind farm installation."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:MonopileTransportSchedule;1']] = 'dtmi:digitaltwins:isa95:MonopileTransportSchedule;1'
+
+    model_id: str = Field(
+        "dtmi:digitaltwins:isa95:MonopileTransportSchedule;1", Literal=True
+    )
     monopile: Monopile
     work_type: WorkTypeEnum
     start_time: str
@@ -152,8 +175,9 @@ class MonopileTransportSchedule(WorkSchedule):
 
 class Vessel(Equipment):
     """A representation for vessel."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:Vessel;1']] = 'dtmi:digitaltwins:isa95:Vessel;1'
-    ID : str
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:Vessel;1", Literal=True)
+    ID: str
     vessel_type: VesselType
     day_rate: float
     responsibility: VesselResponsibility
@@ -163,29 +187,33 @@ class Vessel(Equipment):
     wind_farm: OffshoreWindFarm = None
 
 
- 
 class MonopileState(WorkflowSpecificationNode):
     """A state of a monopile transport for offshore wind farm installation."""
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:MonopileState;1']] = 'dtmi:digitaltwins:isa95:MonopileState;1'
+
+    model_id: str = Field("dtmi:digitaltwins:isa95:MonopileState;1", Literal=True)
     vessel: Vessel
     fabrication_yard: FabricationYard
     monopile_transport_schedule: MonopileTransportSchedule
 
-class GrillageType(EquipmentCapabilityProperty):
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:GrillageType;1']] = 'dtmi:digitaltwins:isa95:GrillageType;1'
-    id: str
-    grillage_type:GrillageTypeEnum
 
-class GrillageCompatibility(ParameterSpecification):
-    model_id: ClassVar[Literal['dtmi:digitaltwins:isa95:GrillageCompatibility;1']] = 'dtmi:digitaltwins:isa95:GrillageCompatibility;1'
+class GrillageType(EquipmentCapabilityProperty):
+    model_id: str = Field("dtmi:digitaltwins:isa95:GrillageType;1", Literal=True)
+    id: str
     grillage_type: GrillageTypeEnum
 
 
 class VesselCapability(EquipmentCapability):
     pass
 
+
 class Decklength(EquipmentCapabilityProperty):
     id: str
     value: str
     value_unit_of_measure: str
 
+
+
+class Availability(EquipmentCapabilityProperty):
+    id: str
+    value: str
+    value_unit_of_measure: str
