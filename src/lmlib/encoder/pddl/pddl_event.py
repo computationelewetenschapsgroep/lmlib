9afa1dfbee037508558fa2ac345b28c61ef14b83
Omik Acharya <omikacharya@gmail.com>
2025-07-20 13:46:19 +0530

    refactor: update PDDLEvent and PDDLProcess to use PDDLPrecondition

--- src/lmlib/encoder/pddl/pddl_event.py
+++ src/lmlib/encoder/pddl/pddl_event.py
@@ -2,20 +2,21 @@ from typing import List
 
 from .pddl_literal import PDDLLiteral
 from .pddl_param import PDDLParam
+from .pddl_precondition import PDDLPrecondition
 
 class PDDLEvent:
     def __init__(self, name: str):
         self.name = name
         self.parameters : list[PDDLParam] = []
-        self.preconditions : list[PDDLLiteral]  = []
+        self.precondition : PDDLPrecondition  = None
         self.effects : list[PDDLLiteral] = []
 
     def add_parameter(self, p: PDDLParam):
         self.parameters.append(p)
         return p
 
-    def add_precondition(self, l: PDDLLiteral):
-        self.preconditions.append(l)
+    def add_precondition(self, l: PDDLPrecondition):
+        self.precondition = l
 
     def add_effect(self, l: PDDLLiteral):
         self.effects.append(l)
@@ -27,16 +28,18 @@ class PDDLEvent:
     def add_parameters(self, p: list[PDDLParam]):
         self.parameters += p
 
-    def add_preconditions(self, l:  List[PDDLLiteral]):
-        self.preconditions += l
+    # def add_precondition(self, l:  List[PDDLLiteral]):
+        # self.preconditions += l
 
 
 
     def to_pddl(self) -> str:
         s = f"\t(:event {self.name}\n"
         s += f"\t\t:parameters ({' '.join(param.to_pddl() for param in self.parameters)})\n"
-        s += "\t\t:precondition (and\n\t\t\t"
-        s += '\n\t\t\t'.join(precondition.to_pddl() for precondition in self.preconditions)
+        # s += "\t\t:precondition (and\n\t\t\t"
+        # s += '\n\t\t\t'.join(precondition.to_pddl() for precondition in self.preconditions)
+        s += "\t\t:precondition "
+        s += self.precondition.to_pddl() + "\n"
         s += "\n\t\t)\n"
         s += "\t\t:effect (and\n\t\t\t"
         s += '\n\t\t\t'.join(effect.to_pddl() for effect in self.effects)
--- src/lmlib/encoder/pddl/pddl_process.py
+++ src/lmlib/encoder/pddl/pddl_process.py
@@ -2,20 +2,21 @@ from typing import List
 
 from .pddl_literal import PDDLLiteral
 from .pddl_param import PDDLParam
+from .pddl_precondition import PDDLPrecondition
 
 class PDDLProcess:
     def __init__(self, name: str):
         self.name = name
         self.parameters : list[PDDLParam] = []
-        self.preconditions : list[PDDLLiteral]  = []
+        self.preconditions : PDDLPrecondition  = None
         self.effects : list[PDDLLiteral] = []
 
     def add_parameter(self, p: PDDLParam):
         self.parameters.append(p)
         return p
 
-    def add_precondition(self, l: PDDLLiteral):
-        self.preconditions.append(l)
+    def add_precondition(self, l: PDDLPrecondition):
+        self.preconditions = l
 
     def add_effect(self, l: PDDLLiteral):
         self.effects.append(l)
@@ -34,8 +35,10 @@ class PDDLProcess:
     def to_pddl(self) -> str:
         s = f"\t(:process {self.name}\n"
         s += f"\t\t:parameters ({' '.join(param.to_pddl() for param in self.parameters)})\n"
-        s += "\t\t:precondition (and\n\t\t\t"
-        s += '\n\t\t\t'.join(precondition.to_pddl() for precondition in self.preconditions)
+        # s += "\t\t:precondition (and\n\t\t\t"
+        # s += '\n\t\t\t'.join(precondition.to_pddl() for precondition in self.preconditions)
+        s += "\t\t:precondition "
+        s += self.precondition.to_pddl() + "\n"
         s += "\n\t\t)\n"
         s += "\t\t:effect (and\n\t\t\t"
         s += '\n\t\t\t'.join(effect.to_pddl() for effect in self.effects)
